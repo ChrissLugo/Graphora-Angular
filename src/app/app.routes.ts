17db401892a083fdb501ac8c57cf6de4afe940f3
Christian Lugo <christianlugo1503@gmail.com>
2025-02-02 19:48:33 -0700

    Página principal terminada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'start',
+    pathMatch: 'full',
+  },
+  {
+    path: 'start',
+    loadComponent: () => import('./business/start/start.component'),
+  },
+  //   {
+  //     path: '',
+  //     loadComponent: () => import('./shared/components/layout/layout.component'),
+  //     children: [
+  //       {
+  //         path: 'home',
+  //         loadComponent: () => import('./business/home/home.component'),
+  //         canActivate: [authGuard],
+  //       },
+  //     ],
+  //   },
+  {
+    path: 'login',
+    loadComponent: () => import('./business/auth/login/login.component'),
+    // canActivate: [AuthenticatedGuard],
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./business/auth/register/register.component'),
+    //canActivate: [AuthenticatedGuard],
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
---
+++ src/app/business/start/start.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-start',
+  imports: [],
+  templateUrl: './start.component.html',
+  styleUrl: './start.component.css',
+})
+export default class StartComponent {
+  constructor(private router: Router) {}
+
+  login() {
+    this.router.navigate(['/login']);
+  }
+
+  register() {
+    this.router.navigate(['/register']);
+  }
+}
