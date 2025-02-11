bbf71080c54f29005bb09e439b1730d64fb8fd94
Christian Lugo <christianlugo1503@gmail.com>
2025-02-11 13:46:33 -0700

    Implementación de router link en los enlaces y formateo correcto.

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -2,45 +2,44 @@ import { Component } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 import { CommonModule, NgClass } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-sidebar',
-  standalone: true,
-  imports: [FormsModule, NgClass, CommonModule],
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css'],
+	selector: 'app-sidebar',
+	standalone: true,
+	imports: [FormsModule, NgClass, CommonModule, RouterLink],
+	templateUrl: './sidebar.component.html',
+	styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
-  selectedFilter: string = '';
-  user: any;
-  notification!: boolean;
+	selectedFilter: string = '';
+	user: any;
+	notification!: boolean;
 
-  constructor(private router: Router) {
-    this.selectedFilter = this.router.url.replace(/\//g, '');
-  }
+	constructor(private router: Router) {
+		this.selectedFilter = this.router.url.replace(/\//g, '');
+	}
 
-  // Metodo para actualizar los datos del usuario en el sidebar
-  getUserData(): void {
-    // this.dataUserService.getUserData().subscribe({
-    //   next: (data) => {
-    //     if (data.image && !data.image.startsWith('blob:')) {
-    //       const blob = this.base64ToBlob(data.image, 'image/jpeg');
-    //       data.image = URL.createObjectURL(blob);
-    //     }
-    //     this.user = data; // Solo un usuario
-    //     //console.log('User data with image converted:', this.user);
-    //   },
-    //   error: (error) => console.error('Error fetching user data:', error),
-    // });
-  }
+	// Metodo para actualizar los datos del usuario en el sidebar
+	getUserData(): void {
+		// this.dataUserService.getUserData().subscribe({
+		//   next: (data) => {
+		//     if (data.image && !data.image.startsWith('blob:')) {
+		//       const blob = this.base64ToBlob(data.image, 'image/jpeg');
+		//       data.image = URL.createObjectURL(blob);
+		//     }
+		//     this.user = data; // Solo un usuario
+		//     //console.log('User data with image converted:', this.user);
+		//   },
+		//   error: (error) => console.error('Error fetching user data:', error),
+		// });
+	}
 
-  selected(selected: string, ruta: string) {
-    this.selectedFilter = selected;
-    this.router.navigate([ruta]);
-  }
+	selected(selected: string): void {
+		this.selectedFilter = selected;
+	}
 
-  logout(): void {
-    // this.authService.logout();
-  }
+	logout(): void {
+		// this.authService.logout();
+	}
 }
