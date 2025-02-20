13378f5cf8c4e3c39906caa548c5db94dcafc7ce
CarlosGarcia20 <cargarcia085@gmail.com>
2025-02-19 19:16:37 -0700

    Modificaciones para el login

--- src/app/business/auth/login/login.component.ts
+++ src/app/business/auth/login/login.component.ts
@@ -1,58 +1,73 @@
 import { Component, inject, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms'; // Importa NgForm
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common'; // Importa CommonModule para *ngIf
+import { LoginService } from '../../../core/services/login/login.service';
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [FormsModule, HttpClientModule, CommonModule], // Añade CommonModule
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    standalone: true,
+    imports: [FormsModule, CommonModule, RouterModule], // Añade CommonModule
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export default class LoginComponent {
-  @ViewChild('loginForm') loginForm!: NgForm; // Referencia al formulario
+    @ViewChild('loginForm') loginForm!: NgForm; // Referencia al formulario
 
-  credentials = {
-    email: '',
-    password: '',
-    rememberMe: false
-  };
+    credentials = {
+        email: '',
+        password: '',
+        rememberMe: false
+    };
 
-  errorMessage = '';
-  isLoading = false;
+    errorMessage = '';
+    isLoading = false;
 
-  private http = inject(HttpClient);
-  private router = inject(Router);
+    constructor(
+        private router: Router,
+        private _loginService: LoginService
+    ) {
 
-  onSubmit() {
-    if (!this.loginForm.valid) {
-      this.errorMessage = 'Por favor completa todos los campos';
-      return; // Detiene la ejecución si el formulario no es válido
     }
 
-    this.isLoading = true;
-    this.errorMessage = '';
-
-    this.http.post<any>('URL_DE_TU_BACKEND/login', this.credentials)
-      .subscribe({
-        next: (response) => {
-          // Guardar token si es necesario
-          if (this.credentials.rememberMe) {
-            localStorage.setItem('authToken', response.token);
-          } else {
-            sessionStorage.setItem('authToken', response.token);
-          }
-          this.router.navigate(['/home']);
-        },
-        error: (err) => {
-          this.errorMessage = err.error.message || 'Error en el login';
-          this.isLoading = false;
-        },
-        complete: () => {
-          this.isLoading = false;
+    onSubmit() {
+        if (!this.loginForm.valid) {
+            alert("Por favor, completa todos los campos");
+            // this.errorMessage = 'Por favor completa todos los campos';
+            return; // Detiene la ejecución si el formulario no es válido
         }
-      });
-  }
+
+        this.isLoading = false;
+        this.errorMessage = '';
+        const formData = this.credentials;
+
+        this._loginService.login(formData).subscribe({
+            next: (responde) => {
+                console.log(responde);
+            },
+            error: (e: HttpErrorResponse) => {
+                console.log(e);
+            }
+        });
+        // this.http.post<any>('URL_DE_TU_BACKEND/login', this.credentials)
+        //     .subscribe({
+        //         next: (response) => {
+        //             // Guardar token si es necesario
+        //             if (this.credentials.rememberMe) {
+        //                 localStorage.setItem('authToken', response.token);
+        //             } else {
+        //                 sessionStorage.setItem('authToken', response.token);
+        //             }
+        //             this.router.navigate(['/home']);
+        //         },
+        //         error: (err) => {
+        //             this.errorMessage = err.error.message || 'Error en el login';
+        //             this.isLoading = false;
+        //         },
+        //         complete: () => {
+        //             this.isLoading = false;
+        //         }
+        //     });
+    }
 }
--- src/app/core/services/login/login.service.ts
+++ src/app/core/services/login/login.service.ts
@@ -1,21 +1,20 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-// // URL de la api
-// import { environment } from '../../../../../environments/environment.development';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+// URL de la api
+import { environment } from '../../../../environments/environment.development';
+@Injectable({
+    providedIn: 'root'
+})
+export class LoginService {
+    private apiURL = environment.API_URL;
+    private loginUrl: string;
 
-// @Injectable({
-//     providedIn: 'root'
-// })
-// export class LoginService {
-//     private apiURL = environment.API_URL;
-//     private loginUrl: string;
+    constructor(private http: HttpClient) {
+        this.loginUrl = "login/";
+    }
 
-//     constructor(private http: HttpClient) {
-//         this.loginUrl = "";
-//     }
-
-//     login(registerForm: void): Observable<any> {
-
-//     }
-// }
+    login(registerForm: any): Observable<any> {
+        return this.http.post<any>(`${this.apiURL}${this.loginUrl}`, registerForm);
+    }
+}
