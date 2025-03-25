c4074ccffbb9476c72c1f5bfeb8512ac3366be7d
CarlosGarcia20 <cargarcia085@gmail.com>
2025-03-24 20:10:14 -0700

    Union de las ramas a la rama principal para poder actualizar otras ramas, ya sean nuevas o viejas

--- src/app/business/auth/login/login.component.ts
+++ src/app/business/auth/login/login.component.ts
@@ -1,9 +1,13 @@
 import { Component, inject, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms'; // Importa NgForm
-import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
 import { Router, RouterModule } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 import { CommonModule } from '@angular/common'; // Importa CommonModule para *ngIf
+
+// Servicios
 import { LoginService } from '../../../core/services/login/login.service';
+import { SwalMessageService } from '../../../services/swal-message.service';
+import Swal from 'sweetalert2';
 
 @Component({
     selector: 'app-login',
@@ -25,28 +29,37 @@ export default class LoginComponent {
 
     constructor(
         private router: Router,
-        private _loginService: LoginService
-    ) {
-
-    }
+        private _loginService: LoginService,
+        private _messageService: SwalMessageService
+    ) { }
 
     onSubmit() {
         if (!this.loginForm.valid) {
-            alert("Por favor, completa todos los campos");
-            // this.errorMessage = 'Por favor completa todos los campos';
+            this._messageService.messageIcon("Por favor, completa todos los campos", "error");
             return; // Detiene la ejecución si el formulario no es válido
         }
+
+        this._messageService.loadMessage();
 
         this.isLoading = false;
         this.errorMessage = '';
         const formData = this.credentials;
 
         this._loginService.login(formData).subscribe({
-            next: (responde) => {
-                console.log(responde);
+            next: (response) => {
+                localStorage.setItem('authToken', response.accessToken);
+                localStorage.setItem('refreshToken', response.refreshToken);
+                this.router.navigate(['/home']);
+
+                Swal.close();
+                // Por si se quiere mostrar un mensaje de exito, aunque no lo veo valido 
+                // this._messageService.messageIcon(response.message, "success").then(() => {
+                //     console.log(response);
+                // });
+
             },
             error: (e: HttpErrorResponse) => {
-                console.log(e);
+                this._messageService.errorResponse(e);
             }
         });
         // this.http.post<any>('URL_DE_TU_BACKEND/login', this.credentials)
