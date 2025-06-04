357d189b9f37e4a70ccf3f7add39ffa644a8efc5
Christian Lugo <christianlugo1503@gmail.com>
2025-06-04 05:54:25 -0700

    Cambio de diseño home

--- src/app/business/home/home.component.ts
+++ src/app/business/home/home.component.ts
@@ -2,18 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { DiagramMakerService } from '../../core/services/diagram/diagram-maker.service';
 import { RouterModule } from '@angular/router';
 import { TemplatesService } from '../../services/API/Templates.Service';
+import {
+	FaIconLibrary,
+	FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import {
+	faArrowRotateLeft,
+	faRotateLeft,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
 	selector: 'app-home',
-	imports: [RouterModule],
+	imports: [RouterModule, FontAwesomeModule],
 	templateUrl: './home.component.html',
 	styleUrl: './home.component.css',
 })
 export default class HomeComponent implements OnInit {
 	constructor(
 		private diagraMaker: DiagramMakerService,
-		public TemplatesService: TemplatesService
-	) {}
+		public TemplatesService: TemplatesService,
+		icons: FaIconLibrary
+	) {
+		icons.addIcons(faArrowRotateLeft);
+	}
 
 	ngOnInit(): void {
 		this.getTemplates();
